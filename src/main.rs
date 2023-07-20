a8cb2941c7508f9ab4a77afc8657671de5bf2350
Icaro S A LopeS <sicaro800@gmail.com>
2023-07-20 06:37:54 -0300

    attempts

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,27 @@ use rand::Rng;
 fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number: {}", secret_number);
     println!("Enter a number");
+
+    let mut _attempts: i32 = 0;
 
     loop {
 
     let mut input = String::new();
-
+    
+    
     io::stdin()
     .read_line(&mut input)
     .expect("Failed to read line");
 
     let input: i32 = input.trim().parse().expect("Please, type a number.");
 
+    _attempts += 1;
+
     println!("You typed: {}", input);
 
     if secret_number == input {
-        println!("You win!");
+        println!("You win! You got it right in {} tries", _attempts);
         break;
     } else if secret_number > input {
         println!("The secret number is bigger!");
